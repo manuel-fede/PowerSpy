9ec99faf2d31282b8e8b3ecce404f4f5583e6c7c
redxef <redxef@senpai.local>
2016-01-28 10:55:00 +0100

    removed ds_store files

---
+++ git_push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+#get the parent folder
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+
+git add *
+printf 'commit msg: '
+read message
+git commit -m "$message"
+git push origin master
